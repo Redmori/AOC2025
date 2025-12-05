c1c02b2d8cadac94451f3a5085a45eead43cd13a
Redmori <72279911+Redmori@users.noreply.github.com>
2025-12-05 17:11:04 +0100

    Day 5 Part 2 attempt

--- src/Day5/FreshRange.java
+++ src/Day5/FreshRange.java
@@ -1,17 +1,31 @@
 package Day5;
 
-public class FreshRange {
+public class FreshRange  implements Comparable<FreshRange>  {
     public long start;
     public long end;
 
-    public FreshRange(String input){
+    public FreshRange(String input) {
         String[] range = input.split("-");
         start = Long.parseLong(range[0]);
         end = Long.parseLong(range[1]);
     }
 
+    public FreshRange(long s, long e){
+        start = s;
+        end = e;
+    }
+
     public boolean isFresh(long value){
         return value >= start && value <= end;
+    }
+
+    public long countVolume(){
+        return end - start - 1;
+    }
+
+    @Override
+    public int compareTo(FreshRange range) {
+        return Long.compare(this.start, range.start);
     }
 
     @Override
--- src/Day5/Main.java
+++ src/Day5/Main.java
@@ -2,11 +2,13 @@ package Day5;
 
 import util.AOC;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        String[] input = AOC.input("src/Day5/input.txt");
+        String[] input = AOC.input("src/Day5/testinput.txt");
 
         ArrayList<FreshRange> ranges = new ArrayList<>();
         int counter = 0;
@@ -29,6 +31,43 @@ public class Main {
         }while(i <= input.length - 1);
 
         System.out.println("Day 5 Part 1: " + counter);
+
+        Collections.sort(ranges);
+
+        System.out.println("=====");
+        for(FreshRange range : ranges){
+            System.out.println(range);
+        }
+        System.out.println("=====");
+
+        long maxValue = ranges.getLast().end;
+
+        long currentValue = 0;
+        ArrayList<FreshRange> invertedRanges = new ArrayList<>();
+        for(FreshRange range : ranges){
+            if(range.start > currentValue){
+                FreshRange newInvertedRange = new FreshRange(currentValue, range.start);
+                invertedRanges.add(newInvertedRange);
+                currentValue = range.end;
+            } else if (range.start <= invertedRanges.getLast().end && range.end > invertedRanges.getLast().end) {
+                //invertedRanges.getLast().end = range.start;
+                currentValue = range.end;
+            }
+        }
+
+        for(FreshRange range : invertedRanges){
+            System.out.println(range);
+        }
+
+        long volume = 0;
+        for(FreshRange range : invertedRanges){
+            volume += range.countVolume();
+        }
+
+
+
+        System.out.println("Day 5 Part 2: " + (maxValue - volume + 1));
+
     }
 
 
